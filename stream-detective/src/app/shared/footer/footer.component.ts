5c748607a367662d746391a5260bd19644f913dd
cjcapone85 <99698474+cjcapone85@users.noreply.github.com>
2022-06-21 21:17:44 -0500

    footer improvements
    added links to each of our github accounts and also links to the most popular streaming services

--- stream-detective/src/app/shared/footer/footer.component.ts
+++ stream-detective/src/app/shared/footer/footer.component.ts
@@ -14,6 +14,38 @@ export class FooterComponent implements OnInit {
     window.location.href='https://www.watchmode.com/';
   }
 
+  githubOne() {
+    window.location.href='https://github.com/marydanderson';
+  }
+
+  githubTwo() {
+    window.location.href='https://github.com/cooper-myatt';
+  }
+
+  githubThree() {
+    window.location.href='https://github.com/cjcapone85';
+  }
+
+  streamingOne() {
+    window.location.href='https://www.netflix.com/';
+  }
+
+  streamingTwo() {
+    window.location.href='https://www.amazon.com/Prime-Video/b?ie=UTF8&node=2676882011';
+  }
+
+  streamingThree() {
+    window.location.href='https://www.crunchyroll.com/';
+  }
+
+  streamingFour() {
+    window.location.href='https://www.disneyplus.com/';
+  }
+
+  streamingFive() {
+    window.location.href='https://www.hbomax.com/';
+  }
+
 
   ngOnInit(): void {
   }
